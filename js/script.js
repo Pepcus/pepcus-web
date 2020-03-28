f574744f972c31d648e831a438ace60608d3dcf3
Amit jain <amit.jain@pepcus.com>
2020-03-29 02:41:48 +0530

    pages added

--- js/script.js
+++ js/script.js
@@ -72,5 +72,98 @@ $(function() {
     }
   });
 
+
+  $('#owl').owlCarousel({
+    loop: true,
+    center: true,
+    items: 1,
+    margin: 0,
+    autoplay: true,
+    dots:true,
+    autoplayTimeout: 8500,
+    smartSpeed: 450,
+  });
+
+
   //owl carousel end
+
+
+  // document.querySelector("html").classList.add('js');
+
+  var fileInput  = document.querySelector( ".input-file" ),
+    button     = document.querySelector( ".input-file-button" ),
+    the_return = document.querySelector(".file-name");
+
+  button.addEventListener( "keydown", function( event ) {
+    if ( event.keyCode == 13 || event.keyCode == 32 ) {
+      fileInput.focus();
+    }
+  });
+  button.addEventListener( "click", function( event ) {
+    fileInput.focus();
+    return false;
+  });
+  fileInput.addEventListener( "change", function( event ) {
+    the_return.innerHTML = this.files[0].name;
+  });
+
 });
+
+
+  var fetchTeamJson = function () {
+  $.ajax({
+    url: 'json/team.json',
+    dataType: 'json',
+    success: function (data) {
+      renderTeamJson(data);
+    },
+    error: function (data) {
+      alert("ERROR: Not received JSON file  " + data);
+      $("#error-wrapper").css("display", "none");
+    }
+  });
+  };
+  fetchTeamJson();
+
+  var renderTeamJson = function (data) {
+    renderThirdTeam(data.thirdTeam);
+    renderThirdTeam1(data.thirdTeam)
+  }
+  var renderThirdTeam = function (data1) {
+    $.each(data1, function(key, value) {
+      var htmlRender =  "<div class='team-members'>" +
+        "<div class='team-member-img'>" +
+        "<div class='team-img-border'>" +
+        "<div class='team-member-image'>" +
+        "<img src='"+value.img1+"' alt='"+value.name+"'>" +
+        "</div>"+
+        "</div>"+
+        "</div>"+
+        "<div class='team-member-info'>" +
+        "<h4 class='team-member-name'>"+value.name+"</h4>" +
+        "<span class='designation'>"+value.role+"</span>" +
+        "</div>"+
+        "</div>";
+      $(htmlRender).appendTo("#teamMembers");
+    });
+  };
+var renderThirdTeam1 = function (data2) {
+  $.each(data2, function(key, value) {
+    var htmlRender =  "<div class='team-members'>" +
+      "<div class='team-member-img'>" +
+      "<div class='team-img-border'>" +
+      "<div class='team-member-image'>" +
+      "<img src='"+value.img1+"' alt='"+value.name+"'>" +
+      "</div>"+
+      "</div>"+
+      "</div>"+
+      "<div class='team-member-info'>" +
+      "<h4 class='team-member-name'>"+value.name+"</h4>" +
+      "<span class='designation'>"+value.role+"</span>" +
+      "</div>"+
+      "</div>";
+    $("#new_team").append(htmlRender);
+  });
+};
+
+
